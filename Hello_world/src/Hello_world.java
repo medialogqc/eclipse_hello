d371bc63d15487ec480b6356c9a6c69ceacd9101
hong-ee <medialogqc@medialog.co.kr>
2015-10-27 18:05:19 +0900

    hotfix branch change a code

--- Hello_world/src/Hello_world.java
+++ Hello_world/src/Hello_world.java
@@ -3,5 +3,6 @@ public class Hello_world {
     public static void main(String args[]) {
         System.out.println("Hello World");
         System.out.println("World's End Dancehall");
+        System.out.println("Hello Dancee World");
     }
 }
